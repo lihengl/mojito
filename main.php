1922f1af38376cffdb6ffb5a3598738902faeeda
Leon Liang <leonlhliang@gmail.com>
2012-10-22 21:13:55 +0800

    cleanup main.php

--- main.php
+++ main.php
@@ -10,42 +10,15 @@ $composer = new MarkupComposer();
 
 $heading = new H1Element("MOJITO");
 
-$img = new ImgElement("img.png", "NOT FOUND");
-$img->attributes->others->set("title", "chiehli and jean");
-$img->attributes->id->set("girls");
-
-$tx1 = new TextElement("中文中文中文");
-$tx2 = new TextElement("Englih English English");
-$lbr = new BrElement();
-
-$pgh = new PElement();
-$pgh->attributes->id->set("document");
-$pgh->attributes->classes->set("main");
-$pgh->attributes->classes->add("focus");
-$pgh->children->add($tx1);
-$pgh->children->add($lbr);
-$pgh->children->add($tx2);
-
-$script = new ScriptElement("script.js");
-
-$div = new DivElement();
-$div->attributes->id->set("main_div");
-$div->children->add($heading);
-$div->children->add($img);
-$div->children->add($pgh);
-
-
 $body = new BodyElement();
-$body->children->add($div);
-$body->children->add($script);
+$body->children->add($heading);
 
-$link = new LinkElement("style.css", "text/css", "stylesheet");
-$title = new TitleElement("testdrive");
-$meta = new MetaElement("charset", "UTF-8");
+$title = new TitleElement("MOJITO");
+$meta_char = new MetaElement("charset", "UTF-8");
+
 $head = new HeadElement();
-$head->children->add($meta);
+$head->children->add($meta_char);
 $head->children->add($title);
-$head->children->add($link);
 
 $html = new HtmlElement();
 $html->children->add($head);
